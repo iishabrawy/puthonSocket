567be0dee7dab53e19591e123bc0049d9d253156
Ismael AlShabrawy <micro0.2050@gmail.com>
2024-03-16 06:16:45 +0200

    first commit

---
+++ server.py
@@ -0,0 +1,24 @@
+import socketio
+import eventlet
+
+# Create a Socket.IO server
+sio = socketio.Server(cors_allowed_origins='*')
+app = socketio.WSGIApp(sio)
+
+# Define event handlers
+@sio.event
+def connect(sid, environ):
+    print('Client connected:', sid)
+
+@sio.event
+def disconnect(sid):
+    print('Client disconnected:', sid)
+
+@sio.event
+def message(sid, data):
+    print('Message received:', data)
+    sio.emit('response', {'data': 'Server received your message'}, room=sid)
+
+# Run the server
+if __name__ == '__main__':
+    eventlet.wsgi.server(eventlet.listen(('localhost', 5000)), app)

37c6f7a419107ce6dbad02a6aae9125946ecde8f
Ismael AlShabrawy <micro0.2050@gmail.com>
2024-03-16 07:20:27 +0200

    first commit

--- server.py
+++ server.py
@@ -20,15 +20,6 @@ def message(sid, data):
     print('Message received in default namespace:', data)
     sio.emit('response', {'data': 'Server received your message in default namespace'}, room=sid)
 
-@sio.event
-def ping(sid):
-    print('Received ping from client in default namespace:', sid)
-    sio.emit('pong', room=sid)
-
-@sio.event
-def pong(sid):
-    print('Received pong from client in default namespace:', sid)
-
 # Define event handlers for the "room" namespace
 @sio.on('connect', namespace='/room')
 def room_connect(sid, environ):
@@ -44,14 +35,10 @@ def room_message(sid, data):
     print('Message received in "room" namespace:', data)
     sio.emit('response', {'data': 'Server received your message in "room" namespace'}, room=sid, namespace='/room')
 
-@sio.on('ping', namespace='/room')
-def room_ping(sid):
-    print('Received ping from client in "room" namespace:', sid)
-    sio.emit('pong', room=sid, namespace='/room')
-
-@sio.on('pong', namespace='/room')
-def room_pong(sid):
-    print('Received pong from client in "room" namespace:', sid)
+# Wildcard event handler for all namespaces
+@sio.event
+def any_event(sid, data):
+    print('Received event from client:', data)
 
 # Run the server
 if __name__ == '__main__':
